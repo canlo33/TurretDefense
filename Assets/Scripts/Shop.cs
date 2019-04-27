7314e1f88baeade1917e767e16ad989349aabff6
Can Lo <canlo33@gmail.com>
2019-04-27 17:15:23 -0400

    Shop Menu UI implementation half way there.

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject turretPanel;
+
+    public void OpenTurretMenu()
+    {
+        Debug.Log("Clicked on Shop Button..");
+
+        if (turretPanel.activeSelf)
+        {
+            Debug.Log("Panel is active, close it..");
+            turretPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Panel is inactive, open it..");
+            turretPanel.SetActive(true);
+        }
+
+    }
+}
